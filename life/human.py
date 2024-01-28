8b0265f2ab3db5524f25536376e6fe06ec91526e
Orbi76 <oorbangabor@gmail.com>
2024-01-28 23:41:08 +0000

    2 Class lab test good

--- life/human.py
+++ life/human.py
@@ -3,7 +3,7 @@ class Human:
     REPRODUCE_ENERGY_COST = 20
     ENERGY_TO_MOVE = 10
 
-    def __int__(self, name: str, age: int = 0, energy: int = 100) -> None:
+    def __init__(self, name: str, age: int = 0, energy: int = 100) -> None:
         self.name = name
         self.age = age
         self.energy = energy
@@ -13,31 +13,35 @@ class Human:
         return f'human(name={self.name}, age={self.age}, energy={self.energy})'
 
     def __str__(self) -> str:
-        return f'{self.name} is {self.age} year old with {self.energy}'
+        return f'{self.name} is {self.age} year old with {self.energy} energy'
 
     def grow(self) -> None:
         self.age += 1
 
     def eat(self, amount: int) -> int:
         self.energy += amount
-        if self.energy > self.MAX_ENERGY:
+        if self.energy >= self.MAX_ENERGY:
             excess_energy = self.energy - self.MAX_ENERGY
             self.energy = self.MAX_ENERGY
             return  excess_energy
-        return max(0, amount - self.MAX_ENERGY + self.energy)
+        else:
+            return self.energy - self.MAX_ENERGY
+           # return max(0, amount - self.MAX_ENERGY + self.energy)
 
 
     def reproduce(self) -> bool:
         if self.energy >= self.REPRODUCE_ENERGY_COST:
             self.energy -= self.REPRODUCE_ENERGY_COST
             return True
-        return False
+        else:
+            return False
 
     def move(self, distance: int) -> bool:
         if self.energy >= self.ENERGY_TO_MOVE:
             self.energy -= distance
             return True
-        return False
+        else:
+            return False
 
 
 
