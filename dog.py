03a6b9cab601d313a730f9a02ed68e3c95a2f7c2
Orbi76 <oorbangabor@gmail.com>
2024-02-09 15:39:34 +0000

    changed location

--- dog.py
+++ dog.py
@@ -1,4 +1,17 @@
 class Dog:
-    def __int__(self, name, age):
+    species = "Bull dog"
+
+    def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def description(self):
+        return f"{self.name} is {self.age} years old."
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+
+kon= Dog('kon', 7)
+print(kon.description())
+print(kon.speak("vau"))
